87ffc1b8d6b7a92e75b2fd94d65f7c2efb9b63dd
Omar Mattos <omarmatt84@gmail.com>
2025-05-05 20:57:19 -0300

    agregamos ejemplo de polimorfismo

---
+++ js/ejercicio05.js
@@ -0,0 +1,18 @@
+class Persona{
+    #nombre
+    #edad
+    #dni
+    #sexo
+    #peso
+    #altura
+    #añoNac
+    constructor(nombre, edad, dni, sexo, peso, altura, añoNac){
+        this.#nombre = nombre
+        this.#edad = edad
+        this.#dni = dni
+        this.#sexo = sexo
+        this.#peso = peso
+        this.#altura = altura
+        this.#añoNac = añoNac
+    }
+}
